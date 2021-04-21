5524fbeffc2b27289663f20a705ebf4d01b62459
rekitto <36672952+rekitto@users.noreply.github.com>
2021-04-21 15:57:25 +0100

    Added Caddy webserver to containers
    * Added new ENV to settings.env
    * Added ./caddy directory folder for building images (will be removed)
    * Added run.sh script to add a new DNS record using Cloudflare

---
+++ caddy/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Replace DOMAIN within Caddyfile
+sed -i "s/REPLACE_DOMAIN/${PHISH_DOMAIN}/g" /etc/caddy/Caddyfile
+
+# Cloudflare zone is the zone which holds the record
+zone=${DOMAIN}
+dnsrecord=${PHISH_DOMAIN}
+
+# Get the current external IP address
+ip=$(curl -s -X GET https://checkip.amazonaws.com)
+
+echo "System Public IP: $ip"
+
+if host $dnsrecord | grep "has address" | grep "$ip"; then
+    echo "$dnsrecord is currently set to $ip; no changes needed"
+else
+    # get the zone id for the requested zone
+    zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+    -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+    echo "[$zone] Zone ID: $zoneid"
+
+    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/dba52117af566ab302d80d49d9403d01/dns_records" \
+    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":120,\"proxied\":false}" | jq
+fi
+
+# Continue with Caddy:
+exec "$@"
