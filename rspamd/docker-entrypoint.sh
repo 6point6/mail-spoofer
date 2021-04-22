2b1ac6a1dd5ec6f30c96ca6b497a7c7cd42bd7ca
rekitto <36672952+rekitto@users.noreply.github.com>
2021-04-22 11:43:48 +0100

    Added support to automatically add DKIM records
    * The rspamd container is able to update DNS records if you provide a Cloudflare API Token
    * Added acme_ca production directory to Caddyfile (not ready yet)

--- rspamd/docker-entrypoint.sh
+++ rspamd/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# DNS Name for DKIM selector e.g. default._domainkey.example.com
+dns_name=${DKIM_TAG}._domainkey.${DOMAIN}
+
 # Set standard logging if no modification exists
 if [ ! -e /etc/rspamd/local.d/logging.inc ] && [ ! -e /etc/rspamd/override.d/logging.inc ];
 then
@@ -20,11 +23,25 @@ sed -i "s/REPLACE_DOMAIN/${DOMAIN}/g" /etc/rspamd/local.d/arc.conf
 sed -i "s/DKIM_TAG/${DKIM_TAG}/g" /etc/rspamd/local.d/arc.conf
 
 echo "############# BEGIN ###############"
-echo " ENTER THE FOLLOWING DNS RECORDS "
 echo " Domain: ${DOMAIN}"
 echo " DKIM Selector: ${DKIM_TAG}"
-echo $(cat /var/lib/rspamd/dkim/${DOMAIN}.${DKIM_TAG}.pub)
-#echo $(rspamadm dkim_keygen -s ${DKIM_TAG} -d ${DOMAIN} -b 2048 -k /var/lib/rspamd/dkim/${DOMAIN}.${DKIM_TAG}.key)
+
+# Get the public key and remove new lines
+record=$(awk -F '"' '{ print $2 $4 }' /var/lib/rspamd/dkim/${DOMAIN}.${DKIM_TAG}.pub | tr -d '\n')
+
+# Get Cloudflare Zone ID
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+  -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo " ADDING TXT DNS RECORD TO CLOUDFLARE "
+
+# Add new TXT type DNS Record
+curl -s -X POST "https://api.cloudflare.com/client/v4/zones/dba52117af566ab302d80d49d9403d01/dns_records" \
+  -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"TXT\",\"name\":\"$dns_name\",\"content\":\"$record\",\"ttl\":120,\"proxied\":false}" | jq
+
 echo "############## END #################"
 
 # Execute the CMD from the Dockerfile:
