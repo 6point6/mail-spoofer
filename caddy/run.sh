1a8f4c49b1a6a535187eb0be069aeeaa324b6f01
rekitto <36672952+rekitto@users.noreply.github.com>
2021-05-21 11:52:04 +0100

    Added support for tracking emails

--- caddy/run.sh
+++ caddy/run.sh
@@ -1,39 +1,53 @@
 #!/bin/sh
 
-# Replace DOMAIN within Caddyfile
-sed -i "s/REPLACE_DOMAIN/${PHISH_DOMAIN}/g" /etc/caddy/Caddyfile
-
-# Cloudflare zone is the zone which holds the record
 zone=${DOMAIN}
-dnsrecord=${PHISH_DOMAIN}
+track_dnsrecord=${TRACK_DOMAIN}
+
+# Replace ALL domain name records within Caddyfile
+sed -i "s/REPLACE_DOMAIN/$zone/"g /etc/caddy/Caddyfile
+sed -i "s/REPLACE_TRACK_DOMAIN/$track_domain/"g /etc/caddy/Caddyfile
 
 # Get the current external IP address
 ip=$(curl -s -X GET https://checkip.amazonaws.com)
 
-echo "System Public IP: $ip"
+update_cloudflare() {
+    # Update record based on zoneid
+    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
+    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"$1\",\"content\":\"$ip\",\"ttl\":120,\"proxied\":false}" | jq
+}
 
-if host $dnsrecord | grep "has address" | grep "$ip"; then
-    echo "$dnsrecord is currently set to $ip; no changes needed"
+# Check Cloudflare API Token is set
+if [[ -z "${CLOUDFLARE_API_TOKEN}" ]]; then
+    echo "YOU NEED TO MANUALLY UPDATE DNS RECORDS"
+    echo "WARNING: CERTIFICATES MAY NOT BE ISSUED PROPERLY!"
+    echo "A root @ record for $zone with value $ip"
+    echo "A record for $track_dnsrecord with value $ip"
+    # Continue with running Caddy
+    exec "$@"
 else
-    # get the zone id for the requested zone
+    echo "Adding records to Cloudflare..."
+    # Get the zone id for the requested zone
     zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
     -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
     -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
-
-    echo "[$zone] Zone ID: $zoneid"
-
-    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
-    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
-    -H "Content-Type: application/json" \
-    --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":120,\"proxied\":false}" | jq
-    
-    # Change @ root record
-    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
-    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
-    -H "Content-Type: application/json" \
-    --data "{\"type\":\"A\",\"name\":\"$zone\",\"content\":\"$ip\",\"ttl\":120,\"proxied\":false}" | jq
-    
 fi
 
+# Check @ root name points to server IP address
+if host $zone | grep "has address" | grep "$ip"; then
+    echo "Record exists."
+else
+    update_cloudflare $zone
+fi
+
+# Check track domain points to server IP address
+if host $track_dnsrecord | grep "has address" | grep "$ip"; then
+    echo "Record Exists"
+else
+    update_cloudflare $track_dnsrecord
+fi
+
+echo "All DNS records are up-to-date."
 # Continue with Caddy:
 exec "$@"
