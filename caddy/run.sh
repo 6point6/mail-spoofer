624cd6d79a065fc467220c3494cb8699c0ba5668
rekitto <36672952+rekitto@users.noreply.github.com>
2021-05-27 17:15:31 +0100

    Added request to update MX record
    * Reduces spam score by 3 points

--- caddy/run.sh
+++ caddy/run.sh
@@ -10,12 +10,25 @@ sed -i "s/REPLACE_TRACK_DOMAIN/$track_dnsrecord/"g /etc/caddy/Caddyfile
 # Get the current external IP address
 ip=$(curl -s -X GET https://checkip.amazonaws.com)
 
-update_cloudflare() {
-    # Update record based on zoneid
+# Get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+-H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+-H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+# Add A record
+add_a_record() {
     curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
     -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
     -H "Content-Type: application/json" \
     --data "{\"type\":\"A\",\"name\":\"$1\",\"content\":\"$ip\",\"ttl\":120,\"proxied\":false}" | jq
+}
+
+# Add MX record
+add_mx_record() {
+    curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records" \
+    -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+    -H "Content-Type: application/json" \
+     --data "{\"type\":\"MX\",\"name\":\"$1\",\"content\":\"$1\",\"ttl\":120,\"priority\":0,\"proxied\":false}" | jq
 }
 
 # Check Cloudflare API Token is set
@@ -28,24 +41,21 @@ if [[ -z "${CLOUDFLARE_API_TOKEN}" ]]; then
     exec "$@"
 else
     echo "Adding records to Cloudflare..."
-    # Get the zone id for the requested zone
-    zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
-    -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
-    -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 fi
 
 # Check @ root name points to server IP address
 if host $zone | grep "has address" | grep "$ip"; then
     echo "Record exists."
 else
-    update_cloudflare $zone
+    add_a_record $zone
+    add_mx_record $zone
 fi
 
 # Check track domain points to server IP address
 if host $track_dnsrecord | grep "has address" | grep "$ip"; then
     echo "Record Exists"
 else
-    update_cloudflare $track_dnsrecord
+    add_a_record $track_dnsrecord
 fi
 
 echo "All DNS records are up-to-date."
